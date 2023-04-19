7358e490a230459caf3fd15036ddf0708f6ea3bd
xun <xun794@gmail.com>
2023-04-20 00:34:49 +0800

    add config

---
+++ .config/hypr/scripts/lock.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+swaylock \
+	--image /home/xun/.config/hypr/assets/lockscreen.png
+--key-hl-color "#FFAACF"
+--ring-color "#F7A4A4"
+--inside-ver-color "#AACB73"
+--indicator \
+	--clock \
+	--grace 2 \
+	--fade-in 0.2 &
