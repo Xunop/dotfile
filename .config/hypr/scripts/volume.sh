fbbecc633628de27ed1ca870bcdd6f629341d0b1
xun <xun794@gmail.com>
2023-05-02 21:40:27 +0800

    add volume.sh

---
+++ .config/hypr/scripts/volume.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+if [ $1 == "up" ]; then
+	amixer set Master 5%+
+	sleep 0.5
+	notify-send "up$(amixer get Master | tail -n 1 | awk -F ' ' '{print $4}' | tr -d '[]%')%"
+else
+	amixer set Master 5%-
+	sleep 0.5
+	notify-send "down$(amixer get Master | tail -n 1 | awk -F ' ' '{print $4}' | tr -d '[]%')%"
+fi
