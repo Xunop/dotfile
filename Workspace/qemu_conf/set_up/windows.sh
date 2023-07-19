a55d4d0528f110571d4143ac14234a877845e391
xun <xun794@gmail.com>
2023-07-19 20:54:07 +0800

    sync

--- Workspace/qemu_conf/set_up/windows.sh
+++ Workspace/qemu_conf/set_up/windows.sh
@@ -20,8 +20,11 @@ qemu-system-x86_64 -drive file=$img_path,format=qcow2,if=virtio \
   -rtc clock=host,base=localtime \
   -m 8G \
   -smp sockets=1,cores=6,threads=1 \
-  -net nic, -net user,hostname=windows \
+  -net nic, -net user,hostname=windows,smb=/home/xun/Workspace/qemu_conf/share \
   -vga virtio \
-  -display sdl,gl=on -name "Windows 10 1709 64 bit" > /home/xun/Workspace/qemu_conf/log/windows.log 2>&1 &
+  -monitor stdio \
+  -display sdl,gl=on -name "Windows 10 1709 64 bit" 
+
+#> /home/xun/Workspace/qemu_conf/log/windows.log 2>&1 &
 
 #-monitor stdio \
