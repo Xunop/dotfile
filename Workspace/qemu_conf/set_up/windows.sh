90dc1885f6600c3c4307555d3af022ad10f2287c
xun <xun794@gmail.com>
2023-07-23 14:18:10 +0800

    modify njupt-login scripts

--- Workspace/qemu_conf/set_up/windows.sh
+++ Workspace/qemu_conf/set_up/windows.sh
@@ -22,8 +22,8 @@ qemu-system-x86_64 -drive file=$img_path,format=qcow2,if=virtio \
   -smp sockets=1,cores=6,threads=1 \
   -net nic, -net user,hostname=windows,smb=/home/xun/Workspace/qemu_conf/share \
   -vga virtio \
-  -onitor stdio \
-  -display sdl,gl=on -name "Windows 10 1709 64 bit" > /home/xun/Workspace/qemu_conf/log/windows.log 2>&1 &
+  -monitor stdio \
+  -display sdl,gl=on -name "Windows 10 1709 64 bit"
 
 #> /home/xun/Workspace/qemu_conf/log/windows.log 2>&1 &
 
