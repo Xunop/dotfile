2fbcf29fb01b9e3ea8ee1edba3e902c41da52dec
xun <xun794@gmail.com>
2023-09-09 17:15:15 +0800

    Modify the paths in the script to facilitate linking & del backup dir

--- .config/xray/my_xray/switch.sh
+++ .config/xray/my_xray/switch.sh
@@ -2,17 +2,21 @@
 
 declare -A nodes
 
+dir="/home/xun/.config/xray/my_xray/"
+private="${dir}private"
+routing="${dir}03_routing.json"
+
 list() {
-  node=$(grep '_uuid' private | wc -l)
+  node=$(grep '_uuid' ${private} | wc -l)
   for i in $(seq 1 1 $node); do
     # list all nodes
     #set -x
-    lnico=$(grep '_uuid' private | awk -F: '{print $2}' | awk -F_ '{print $1}' | sed -n "$i"p)
-    larea=$(grep '_uuid' private | awk -F: '{print $2}' | awk -F_ '{print $2}' | sed -n "$i"p)
+    lnico=$(grep '_uuid' ${private} | awk -F: '{print $2}' | awk -F_ '{print $1}' | sed -n "$i"p)
+    larea=$(grep '_uuid' ${private} | awk -F: '{print $2}' | awk -F_ '{print $2}' | sed -n "$i"p)
     echo "$i:${lnico}-proxy-${larea}"
     tmp="${lnico}-proxy-${larea}"
     nodes[$i]=$tmp
-    sed -i "s/${tmp}/proxy-tag/g" 03_routing.json
+    sed -i "s/${tmp}/proxy-tag/g" ${routing}
   done
 }
 
@@ -22,10 +26,10 @@ switch() {
   echo "Please input the number of node:"
   read num
   if [ $num -gt 0 ]; then
-    nico=$(grep '_uuid' private | awk -F: '{print $2}' | awk -F_ '{print $1}' | sed -n "$num"p)
-    area=$(grep '_uuid' private | awk -F: '{print $2}' | awk -F_ '{print $2}' | sed -n "$num"p)
+    nico=$(grep '_uuid' ${private} | awk -F: '{print $2}' | awk -F_ '{print $1}' | sed -n "$num"p)
+    area=$(grep '_uuid' ${private} | awk -F: '{print $2}' | awk -F_ '{print $2}' | sed -n "$num"p)
     nodeN="${nico}-proxy-${area}"
-    sed -i "s/proxy-tag/${nodeN}/g" 03_routing.json
+    sed -i "s/proxy-tag/${nodeN}/g" ${routing}
     echo "Switch to node $num"
   else
     echo "Please input the right number"
@@ -35,14 +39,14 @@ switch() {
 end() {
   for node in ${nodes[@]}; do
     #set -x
-    sed -i "s/${node}/proxy-tag/g" 03_routing.json
+    sed -i "s/${node}/proxy-tag/g" ${routing}
   done
 }
 
 n_node=$(awk -F':' '/\-proxy-/ {print $2; exit}' /etc/xray/03_routing.json | awk '{print $1}')
 echo "Current node is ${n_node}"
 switch
-sudo ./sync.sh -s
+sudo ${dir}sync.sh -s
 sudo systemctl restart xray.service
 end
 
--- .config/xray/my_xray/sync.sh
+++ .config/xray/my_xray/sync.sh
@@ -5,16 +5,29 @@ declare -A priv_map
 # key is replace_s, value is private info
 declare -A map_priv
 
-conf_file=(00_log 01_api 02_dns 03_routing 04_policy 05_inbounds 06_outbounds 07_transport 08_stats 09_reverse)
+conf_dir="/home/xun/.config/xray/my_xray/"
+back_dir="${conf_dir}backup/"
+private="${dir}private"
+routing="${dir}03_routing.json"
+
+file=(00_log 01_api 02_dns 03_routing 04_policy 05_inbounds 06_outbounds 07_transport 08_stats 09_reverse)
+declare -A conf_file
+declare -A back_file
+declare -A xray_file
+for BASE in ${file[@]}; do
+  conf_file[$BASE]="${conf_dir}${BASE}.json"
+  back_file[$BASE]="${back_dir}${BASE}.json"
+  xray_file[$BASE]="/etc/xray/${BASE}.json"
+done
 
 # backup file
 backup() {
-  for BASE in ${conf_file[@]}; do 
-    # if file not exist, create it
-    if [ ! -e "/home/xun/.config/xray/my_xray/backup/$BASE.json" ]; then
-      echo '{}' > "/home/xun/.config/xray/my_xray/backup/$BASE.json"
+  for BASE in ${file[@]}; do
+    # if backup file not exist, create it
+    if [ ! -e ${back_file[$BASE]} ]; then
+      echo '{}' > ${back_file[$BASE]}
     fi
-    /bin/cp "/etc/xray/$BASE.json" "/home/xun/.config/xray/my_xray/backup/"
+    /bin/cp ${xray_file[$BASE]} "/home/xun/.config/xray/my_xray/backup/"
   done
 }
 
@@ -25,22 +38,23 @@ read_info() {
     replace_s=$(echo $line | awk -F: '{print $2}')
     priv_map[$priv_info]=$replace_s
     map_priv[$replace_s]=$priv_info
-  done < private
+  done < ${private}
 }
 
 # replace private info
 replace_info() {
-  for BASE in ${conf_file[@]}; do 
+  for BASE in ${file[@]}; do
     for key in ${!priv_map[@]}; do
-      sed -i "s/$key/${priv_map[$key]}/g" "$BASE.json"
+      sed -i "s/$key/${priv_map[$key]}/g" ${conf_file[$BASE]}
+      sed -i "s/$key/${priv_map[$key]}/g" ${back_file[$BASE]}
     done
   done
 }
 
 info_replace() {
-  for BASE in ${conf_file[@]}; do 
+  for BASE in ${file[@]}; do
     for key in ${!map_priv[@]}; do
-      sed -i "s/$key/${map_priv[$key]}/g" "$BASE.json"
+      sed -i "s/$key/${map_priv[$key]}/g" ${conf_file[$BASE]}
     done
   done
 }
@@ -54,16 +68,16 @@ sync_xray() {
   fi
   
   echo "please make sure your xray run command is '/usr/bin/xray run -confdir /etc/xray"
-  sed -i "s/proxy-tag/xun-proxy-jp/g" 03_routing.json
-  for BASE in ${conf_file[@]}; do 
+  sed -i "s/proxy-tag/xun-proxy-jp/g" ${conf_dir}03_routing.json
+  for BASE in ${file[@]}; do 
     # if file not exist, create it
-    if [ ! -e "/etc/xray/$BASE.json" ]; then
+    if [ ! -e ${xray_file[$BASE]} ]; then
       echo 'file not exist, create it'
-      echo '{}' > "/etc/xray/$BASE.json"
+      echo '{}' > ${xray_file[$BASE]}
     fi
-    cat "$BASE.json" > "/etc/xray/$BASE.json"
+    cat ${conf_file[$BASE]} > ${xray_file[$BASE]}
   done
-  sed -i "s/xun-proxy-jp/proxy-tag/g" 03_routing.json
+  sed -i "s/xun-proxy-jp/proxy-tag/g" ${conf_dir}03_routing.json
   echo "done. but you need to restart xray.service"
   echo "sudo systemctl restart xray.service"
 }
@@ -97,6 +111,7 @@ parse_args() {
         shift
         ;;
       -s|--sync)
+        backup
         info_replace
         sync_xray
         replace_info
@@ -116,7 +131,5 @@ parse_args() {
 
 # main
 read_info
-
-backup
 
 parse_args "$@"
